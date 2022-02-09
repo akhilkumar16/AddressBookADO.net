5ff1efb0a1a614d37b97307a09dbe81bdfca6550
Your Name <akhilkumarchandragiri@gmail.com>
2022-02-09 13:23:23 +0530

    [Akhil] uc1

---
+++ AddressBookADO.net/AddressBookManagement.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookADO.net 
+{ 
+    public class AddressBookManagement
+    {
+        /* UC1:- Address Book ServiceDB
+                 - Use SQL Client to create DB and DB Records.
+        */
+        //Specifying the connection string from the sql server connection.
+        public static string connectionString = @"(localdb)\MSSQLLocalDB;Initial Catalog=AddressBooksystem;Integrated Security=True";
+        // Establishing the connection using the Sqlconnection.
+        SqlConnection connection = new SqlConnection(connectionString);
+        public void DataBaseConnection()
+        {
+            try
+            {
+                DateTime now = DateTime.Now; //create object DateTime class //DateTime.Now class access system date and time 
+                connection.Open(); // open connection
+                using (connection)  //using SqlConnection
+                {
+                    Console.WriteLine($"Connection is created Successful {now}"); //print msg
+                }
+                connection.Close(); //close connection
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
